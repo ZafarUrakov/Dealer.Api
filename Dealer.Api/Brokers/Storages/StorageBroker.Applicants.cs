ee2300d83a802bb3a843f02be6413f4f6c715c87
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-14 16:04:53 +0900

    POC: Import Applicants(WIP)

--- Dealer.Api/Brokers/Storages/StorageBroker.Applicants.cs
+++ Dealer.Api/Brokers/Storages/StorageBroker.Applicants.cs
@@ -3,16 +3,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using System;
 using Dealer.Api.Models.Groups;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dealer.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
+        public DbSet<Applicant> Applicants { get; set; }
         public async ValueTask<Applicant> InsertApplicantAsync(Applicant applicant) =>
             await InsertAsync(applicant);
 
         public IQueryable<Applicant> SelectAllApplicants() =>
-            SelectAll<Applicant>();
+            SelectAll<Applicant>().AsQueryable();
 
         public async ValueTask<Applicant> SelectApplicantByIdAsync(Guid applicantId) =>
             await SelectAsync<Applicant>(applicantId);
--- Dealer.Api/Program.cs
+++ Dealer.Api/Program.cs
@@ -5,15 +5,14 @@ namespace Dealer.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => 
+                    webBuilder.UseStartup<Startup>());
         }
     }
 }
